5ee3b843abffa11654dae392537d5eab5ce2fdf0
pamd7 <pamd72120@att.net>
2018-07-26 16:14:13 -0500

    Comparison Exercise

---
+++ ch05/Comparison.java
@@ -0,0 +1,18 @@
+public class Comparison
+{
+    public static void main(String[] args)
+    {
+        String txt = "Fantastic";
+        String lang = "Java";
+        boolean state = (txt ==lang);
+        System.out.println("String Equality Test:" + state);
+        state = (txt != lang);
+        System.out.println("String Inequality Test" + state);
+        int dozen = 12;
+        int score = 20;
+        state = (dozen > score);
+        System.out.println("Greater Than Test:" + state);
+        state = (dozen < score);
+        System.out.println("Less Than Test" + state);
+    }
+}
